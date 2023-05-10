cca6ae3c373cf0a96b9272890dce16616da5f6dc
shawn8728 <wang61107@gmail.com>
2023-05-10 07:29:09 -0700

    Modify class with className

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,37 +5,37 @@ import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 export default function Footer() {
   return (
-    <footer class="p-4 sm:p-6">
-      <div class="p-4 sm:p-6 mx-auto max-w-screen-xl">
-        <div class="md:flex md:justify-between"></div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="p-4 sm:p-6">
+      <div className="p-4 sm:p-6 mx-auto max-w-screen-xl">
+        <div className="md:flex md:justify-between"></div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2022{" "}
-            <a href="#" target="_blank" class="hover:underline">
+            <a href="#" target="_blank" className="hover:underline">
               Shawn Wang™
             </a>
             . All Rights Reserved.
           </span>
-          <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a
               href="https://www.linkedin.com/in/shawn8728"
               target="_blank"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <FontAwesomeIcon icon={faLinkedin} />
             </a>
             <a
               href="https://github.com/shawn8728"
               target="_blank"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <FontAwesomeIcon icon={faGithub} />
             </a>
             <a
               href="#"
               target="_blank"
-              class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+              className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
             >
               <FontAwesomeIcon icon={faEnvelope} />
             </a>
