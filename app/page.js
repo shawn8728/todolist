59bc2018e6717734823e024f6875dd358ca0ed90
shawn8728 <wang61107@gmail.com>
2023-05-10 05:30:44 -0700

    Setup and install brands icon

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
-import Image from 'next/image'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+
+// Prevent fontawesome from dynamically adding its css
+config.autoAddCss = false
 
 export default function Home() {
   return (
