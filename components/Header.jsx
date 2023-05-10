5b33569decaa0764dffad3e9072006866e491f86
shawn8728 <wang61107@gmail.com>
2023-05-10 08:16:46 -0700

    Update header width and class to className

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,12 +7,12 @@ import { faBars, faClipboardCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-    <header class="px-4 lg:px-6 py-2.5 border-b-2">
-      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+    <header>
+      <div className="flex flex-wrap justify-between items-center border-b-2 px-4 lg:px-6 py-2.5 mx-auto max-w-screen-xl w-full">
         {/* <!-- Brand --> */}
-        <a href="/" class="flex items-center">
+        <a href="/" className="flex items-center">
           <FontAwesomeIcon icon={faClipboardCheck} />
-          <span class="ml-2 self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             Todolist
           </span>
         </a>
