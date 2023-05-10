a81dd8b48b7912704a4e1a6e6127b780060995b8
shawn8728 <wang61107@gmail.com>
2023-05-10 05:08:37 -0700

    Setup header and footer

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
 import Image from 'next/image'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    </main>
+    <div className="flex min-h-screen flex-col relative">
+      <main className="flex-1">
+      <Header />
+      </main>
+      <Footer />
+    </div>
   )
 }
