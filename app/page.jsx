b127e91a7e5602c412e7aad13642994a09f3f917
shawn8728 <wang61107@gmail.com>
2023-05-10 20:04:53 -0700

    Add todocard

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,12 @@
-export default function Home() {
-  return <div>
-    <h1>Home</h1>
-  </div>;
+import React from "react";
+import ToDoCard from "@/components/ToDoCard";
+
+export default function HomePage() {
+  return (
+    <div className="py-8 px-4 mx-auto w-full max-w-prose">
+      <div className="grid gap-8">
+        <ToDoCard />
+      </div>
+    </div>
+  );
 }
---
+++ components/ToDoCard.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+import { Card } from "flowbite-react";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function ToDoCard(props) {
+  return (
+    <Card>
+      <div className="flex-1">
+        Todoitem
+      </div>
+      <div className="flex justify-end items-end mt-4 gap-5">
+        <FontAwesomeIcon icon={faPenToSquare} className="hover:scale-125 cursor-pointer" size="lg" />
+        <FontAwesomeIcon icon={faTrash} className="hover:scale-125 cursor-pointer" size="lg" />
+      </div>
+    </Card>
+  );
+}
