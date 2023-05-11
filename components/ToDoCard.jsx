55301862c4fa9ee8d87f3e090aa3d24fc626886e
shawn8728 <wang61107@gmail.com>
2023-05-10 21:49:56 -0700

    Change todocard to client component

--- components/ToDoCard.jsx
+++ components/ToDoCard.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { React, useState } from "react";
 import { Card } from "flowbite-react";
 import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,12 +8,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function ToDoCard(props) {
   return (
     <Card>
-      <div className="flex-1">
-        Todoitem
-      </div>
+      <div className="flex-1">{props.value}</div>
       <div className="flex justify-end items-end mt-4 gap-5">
-        <FontAwesomeIcon icon={faPenToSquare} className="hover:scale-125 cursor-pointer" size="lg" />
-        <FontAwesomeIcon icon={faTrash} className="hover:scale-125 cursor-pointer" size="lg" />
+        <FontAwesomeIcon
+          icon={faPenToSquare}
+          className="hover:scale-125 cursor-pointer"
+          size="lg"
+        />
+        <FontAwesomeIcon
+          icon={faTrash}
+          className="hover:scale-125 cursor-pointer"
+          size="lg"
+        />
       </div>
     </Card>
   );
