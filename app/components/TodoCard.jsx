e6362591a14f193aa13d47e7e69a00c010457b3e
shawn8728 <wang61107@gmail.com>
2023-05-12 08:35:38 -0700

    Change edit state icon

--- app/components/TodoCard.jsx
+++ app/components/TodoCard.jsx
@@ -5,6 +5,7 @@ import { React, useState } from "react";
 import { TextInput } from "flowbite-react";
 import { faPenToSquare, faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function ToDoCard(props) {
   const { handleDelete, handleEdit, name, id } = props;
@@ -48,12 +49,21 @@ export default function ToDoCard(props) {
         )}
       </div>
       <div className="flex items-center gap-4 p-2">
-        <FontAwesomeIcon
-          icon={faPenToSquare}
-          onClick={handleEditClick}
-          className="hover:scale-125 cursor-pointer"
-          size="lg"
-        />
+        {edit ? (
+          <FontAwesomeIcon
+            icon={faCheck}
+            onClick={handleEditClick}
+            className="hover:scale-125 cursor-pointer"
+            size="lg"
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faPenToSquare}
+            onClick={handleEditClick}
+            className="hover:scale-125 cursor-pointer"
+            size="lg"
+          />
+        )}
         <FontAwesomeIcon
           icon={faTrashCan}
           onClick={handleDeleteClick}
