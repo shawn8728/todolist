ead9f7ba66c43ab8a3e094cd50fba9751f252589
shawn8728 <wang61107@gmail.com>
2023-05-12 06:54:14 -0700

    Add route for user already logged in

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import Login from '@/components/Login'
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+import Login from "@/app/components/Login";
+
+import { useAuth } from "@/context/AuthContext";
 
 export default function page() {
-  return (
-    <Login />
-  )
+  const { currentUser } = useAuth();
+  const router = useRouter();
+
+  currentUser && router.push("/dashboard");
+
+  return <Login />;
 }
