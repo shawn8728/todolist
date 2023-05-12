693c6b10ad3abcad07da66e00fc5a53fc5cfcd78
shawn8728 <wang61107@gmail.com>
2023-05-12 06:52:39 -0700

    Setup authprovider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,31 +1,31 @@
+"use client";
+
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+
+import { AuthProvider } from "@/context/AuthContext";
 
 // Prevent fontawesome from dynamically adding its css
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "To Do List",
-  description: "A simple to do list app",
-  // image: "/public/og.png",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex min-h-screen flex-col relative">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
+        <AuthProvider>
+          <div className="flex min-h-screen flex-col relative">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
+        </AuthProvider>
       </body>
     </html>
   );
