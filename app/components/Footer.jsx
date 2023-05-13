77b8f8506bcc4294bb75625c90fe6ea504f03b2b
shawn8728 <wang61107@gmail.com>
2023-05-13 07:46:20 -0700

    Update footer mark

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2022{" "}
+            © 2023{" "}
             <a href="https://www.linkedin.com/in/shawn8728" target="_blank" className="hover:underline">
               Shawn Wang™
             </a>
